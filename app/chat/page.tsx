0bc5275c9b508249afb0906ae985f2c80263aa76
plykung <58039748+plykung@users.noreply.github.com>
2025-09-13 23:59:25 +0700

    feat: add mic icon

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -253,13 +253,24 @@ function ChatPage() {
                 className="p-4 bg-white border-t flex gap-2 items-center"
                 autoComplete="off"
             >
-                <input
-                    type="text"
-                    className="flex-1 border rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
-                    placeholder="Type your message..."
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                />
+                <div className="relative flex-1">
+                    <input
+                        type="text"
+                        className="w-full border rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300 pr-10"
+                        placeholder="Type your message..."
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button
+                        type="button"
+                        className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400 hover:text-blue-500 focus:outline-none"
+                        tabIndex={-1}
+                        aria-label="Voice input"
+                    >
+                        {/* New microphone icon */}
+                        <svg xmlns="http://www.w3.org/2000/svg" height="21px" version="1.1" viewBox="0 0 14 21" width="14px"><title /><desc /><defs /><g fill="none" fillRule="evenodd" id="Page-1" stroke="none" stroke-width="1"><g fill="#000000" id="Icons-AV" transform="translate(-3.000000, -43.000000)"><g id="mic" transform="translate(3.000000, 43.500000)"><path d="M7,12 C8.7,12 10,10.7 10,9 L10,3 C10,1.3 8.7,0 7,0 C5.3,0 4,1.3 4,3 L4,9 C4,10.7 5.3,12 7,12 L7,12 Z M12.3,9 C12.3,12 9.8,14.1 7,14.1 C4.2,14.1 1.7,12 1.7,9 L0,9 C0,12.4 2.7,15.2 6,15.7 L6,19 L8,19 L8,15.7 C11.3,15.2 14,12.4 14,9 L12.3,9 L12.3,9 Z" id="Shape" /></g></g></g></svg>
+                    </button>
+                </div>
                 <button
                     type="submit"
                     className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
